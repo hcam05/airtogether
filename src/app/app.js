c43fef94e3d05c092fa00cf3882965b9227e35fa
ChrisKim <ckimchris@Chris-Kims-MacBook-Pro-2.local>
2017-12-04 20:07:25 -0800

    navbar components barfed out

--- src/app/app.js
+++ src/app/app.js
@@ -1,13 +1,21 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import NavBar from './NavBar/NavBar';
+import AutoCompleteExampleSimple from './NavBar/Search';
+import DrawerOpenRightExample from './NavBar/Drawer';
 const App = (props) => {
   const { isMobile } = props.device;
 
   return (
-    <div>
-      <h1>hello world {isMobile ? 'mobile' : 'desktop'}</h1>
-    </div>
+    <MuiThemeProvider>
+      <div>
+        <NavBar />
+        <AutoCompleteExampleSimple />
+        <DrawerOpenRightExample />
+      </div>
+    </MuiThemeProvider>
   );
 };
 

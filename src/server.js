38798f9aa0408511d26dfc58805433a1523d2578
hcam05 <hcam05@gmail.com>
2017-12-05 15:44:54 -0800

    initial database setup

--- src/server.js
+++ src/server.js
@@ -9,11 +9,20 @@ import { enableMobile } from './app/modules/device';
 import App from './app/app';
 import template from './template';
 
+//import database configuration
 import db from './db/dbConfig';
+//import database controllers
+// import {addUser} from './controllers/event-controllers';
 
 //database connection
 db.connect()
-  .then(obj => db.any('SELECT * FROM users'))
+  .then(obj => {
+    console.log('connected to db')
+    // db.query('SELECT * FROM "Users"')
+    // .then((resp) => console.log(resp))
+    // db.query('SELECT * FROM "Services"')
+    // .then((resp) => console.log(resp))
+  })
   .catch(error => console.log('error connecting'));
 
 const server = express();
@@ -38,6 +47,29 @@ server.get('/', (req, res) => {
     initialState: JSON.stringify(state)
   }));
 });
+
+//server routes
+
+//Create New Routes
+server.post('/createuser', (req, res) => {
+  console.log('hello create user')
+  res.send('hello add user');
+})
+//Join a Group
+server.post('/joingroup', (req, res) => {
+  console.log('hello join group')
+  res.send('hello join group');
+})
+//Leave a Group
+server.post('/leavegroup', (req, res) => {
+  console.log('hello leave group')
+  res.send('hello leave group');
+})
+//Home
+server.get('/home', (req, res) => {
+  console.log('hello home')
+  res.send('hello home');
+})
 
 server.listen(8080, () => {
   console.log('listening on port 8080');
